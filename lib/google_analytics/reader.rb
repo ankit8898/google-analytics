14694a09ea68cd53c023308f12438b6c7a61fbc8
Ankit Gupta <ankit.gupta8898@gmail.com>
2014-03-01 15:02:15 -0500

    Passing metric as opts

--- lib/google_analytics/reader.rb
+++ lib/google_analytics/reader.rb
@@ -21,39 +21,39 @@ module GoogleAnalytics
 
 
 		def visits
-			fetcher('ga:visits').data.totalsForAllResults.send('ga:visits')
+			fetcher({metric: 'ga:visits'}).data.totalsForAllResults.send('ga:visits')
 		end
 
 		def unique_visitors
-			fetcher('ga:visitors').data.totalsForAllResults.send('ga:visitors')
+			fetcher({metric: 'ga:visitors'}).data.totalsForAllResults.send('ga:visitors')
 		end
 
 		def bounce_rate
-			fetcher('ga:visitBounceRate').data.totalsForAllResults.send('ga:visitBounceRate').to_f.round(2)
+			fetcher({metric: 'ga:visitBounceRate'}).data.totalsForAllResults.send('ga:visitBounceRate').to_f.round(2)
 		end
 
 		def page_views
-			fetcher('ga:pageviews').data.totalsForAllResults.send('ga:pageviews')
+			fetcher({metric: 'ga:pageviews'}).data.totalsForAllResults.send('ga:pageviews')
 		end
 
 		def avg_visit_duration
-			fetcher('ga:avgTimeOnSite').data.totalsForAllResults.send('ga:avgTimeOnSite').to_f.round(2)
+			fetcher({metric: 'ga:avgTimeOnSite'}).data.totalsForAllResults.send('ga:avgTimeOnSite').to_f.round(2)
 		end
 
 		def pages_per_visit
-		   fetcher('ga:pageviewsPerVisit').data.totalsForAllResults.send('ga:pageviewsPerVisit').to_f.round(2)
+		   fetcher({metric: 'ga:pageviewsPerVisit'}).data.totalsForAllResults.send('ga:pageviewsPerVisit').to_f.round(2)
 		end
 
 		def percent_new_visits
-		   fetcher('ga:percentNewVisits').data.totalsForAllResults.send('ga:percentNewVisits').to_f.round(2)
+		   fetcher({metric: 'ga:percentNewVisits'}).data.totalsForAllResults.send('ga:percentNewVisits').to_f.round(2)
 		end
 
-		def fetcher(metric)
+		def fetcher(options = {})
 			@client.execute(:api_method => @api[:analytics].data.ga.get, :parameters => { 
 				'ids' => "ga:" + @@profileID, 
 				'start-date' => @start_date,
 				'end-date' => @end_date,
-				'metrics' => metric
+				'metrics' => options[:metric]
 				})
 		end
 	end
